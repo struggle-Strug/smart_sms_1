cbd7354d2a3983cd5f1ca55e9beee591a726967e
DESKTOP-7RD4RS0\ao10v <ao10v01oa@gmail.com>
2024-10-10 11:16:56 +0900

    fix

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-// import React from 'react';
-import React, { useState, useRef, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import { useLocation } from 'react-router-dom';
-import TaxesIndex from './Home/Taxes';
-
-
-function Home() {
-    const [isOpen, setIsOpen] = useState(false);
-    const dropdownRef = useRef(null);
-    const location = useLocation();
-
-=======
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import SalesTaxSettingsIndex from './SalesTaxSettings/index';
@@ -20,7 +6,6 @@ import { Navigate } from 'react-router-dom';
 
 function Home() {
     const location = useLocation();
->>>>>>> main
     return (
         <>
             {
@@ -102,89 +87,11 @@ function Home() {
                         </div>
                     </>
                 </div>
-<<<<<<< HEAD
-                <div className='grid grid-col-12 mb-4'>
-                    <div className='col-start-1 col-end-2 text-gray-600'>2024年10月10日</div>
-                    <div className='col-start-2 col-end-12 pl-4'>タイトルが入ります。タイトルが入ります。タイトルが入ります。タイトルが入ります。</div>
-                    <div className='col-start-12 col-end-13'><img src='/Icon.png' width={24} /></div>
-                </div>
-                <div className='grid grid-col-12 mb-4'>
-                    <div className='col-start-1 col-end-2 text-gray-600'>2024年10月10日</div>
-                    <div className='col-start-2 col-end-12 pl-4'>タイトルが入ります。タイトルが入ります。タイトルが入ります。タイトルが入ります。</div>
-                    <div className='col-start-12 col-end-13'><img src='/Icon.png' width={24} /></div>
-                </div>
-                <div className='grid grid-col-12 mb-4'>
-                    <div className='col-start-1 col-end-2 text-gray-600'>2024年10月10日</div>
-                    <div className='col-start-2 col-end-12 pl-4'>タイトルが入ります。タイトルが入ります。タイトルが入ります。タイトルが入ります。</div>
-                    <div className='col-start-12 col-end-13'><img src='/Icon.png' width={24} /></div>
-                </div>
-                <div className='grid grid-col-12'>
-                    <div className='col-start-1 col-end-2 text-gray-600'>2024年10月10日</div>
-                    <div className='col-start-2 col-end-12 pl-4'>タイトルが入ります。タイトルが入ります。タイトルが入ります。タイトルが入ります。</div>
-                    <div className='col-start-12 col-end-13'><img src='/Icon.png' width={24} /></div>
-                </div>
-            </div>
-            <div className='text-3xl font-bold my-9'>
-                データ設定
-            </div>
-            <div className='flex items-center'>
-                <div className='flex items-center py-3 px-4 border rounded'>
-                    <div className='font-bold'>バックアップ設定</div>
-                    <div><img src='/Icon.png' width={24} /></div>
-                </div>
-                <div className='flex items-center py-3 px-4 border rounded ml-14'>
-                    <div className='font-bold'>データコンバート</div>
-                    <div><img src='/Icon.png' width={24} /></div>
-                </div>
-            </div>
-            <div className='text-3xl font-bold my-9'>
-                ファイル管理
-            </div>
-            <div className=''>
-                <div className='flex items-center'>
-                    <div className='flex items-center py-3 px-4 border rounded'>
-                        <div className='font-bold'>管理者設定</div>
-                        <div><img src='/Icon.png' width={24} /></div>
-                    </div>
-                    <div className='flex items-center py-3 px-4 border rounded ml-14'>
-                        <div className='font-bold'>POS連携</div>
-                        <div><img src='/Icon.png' width={24} /></div>
-                    </div>
-                    <div className='flex items-center py-3 px-4 border rounded ml-14'>
-                        <div className='font-bold'>消費税設定</div>
-                        <div><img src='/Icon.png' width={24} /></div>
-                    </div>
-                    <div className='flex items-center py-3 px-4 border rounded ml-14'>
-                        <div className='font-bold'>使用者設定</div>
-                        <div><img src='/Icon.png' width={24} /></div>
-                    </div>
-                    <div className='flex items-center py-3 px-4 border rounded ml-14'>
-                        <div className='font-bold'>銀行API連携</div>
-                        <div><img src='/Icon.png' width={24} /></div>
-                    </div>
-                </div>
-                <div className='flex items-center mt-6'>
-                    
-                </div>
-            </div>
-
-            {/* 消費税テスト */}
-            <div className='text-3xl font-bold my-9'>
-                消費税テスト
-            </div>
-            <div className='flex'>
-                <Routes>
-                    <Route path="taxes/*" element={<TaxesIndex />} />
-                </Routes>
-            </div>
-        </div>
-=======
             }
             <Routes>
                 <Route path="sales-tax-settings/*" element={<SalesTaxSettingsIndex />} />
             </Routes>
         </>
->>>>>>> main
     )
 }
 
