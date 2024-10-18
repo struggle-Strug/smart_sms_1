f120f6f20fbf5a515c00cb619109da6fe7741422
esakiryota <esaki1217@gmail.com>
2024-10-18 10:03:10 +0900

    修正

--- src/Procurements/Statements/PurchaseOrderStatements/index.js
+++ src/Procurements/Statements/PurchaseOrderStatements/index.js
@@ -2,7 +2,6 @@ import React, { useState, useRef, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import CustomSelect from '../../../Components/CustomSelect';
-import SettingsDialog from '../../../Components/Statements/SettingsDialog';
 
 const { ipcRenderer } = window.require('electron');
 
@@ -43,21 +42,52 @@ function Index() {
 
     useEffect(() => {
         ipcRenderer.send('load-purchase-order-details');
-        
+
         const handleLoadDetails = (event, data) => setPurchaseOrderDetails(data);
         const handleSearchResult = (event, data) => {
-            
+
             setPurchaseOrderDetails(data);
         };
-    
+
         ipcRenderer.on('load-purchase-order-details', handleLoadDetails);
         ipcRenderer.on('search-purchase-order-details-result', handleSearchResult);
-    
+
         return () => {
             ipcRenderer.removeListener('load-purchase-order-details', handleLoadDetails);
             ipcRenderer.removeListener('search-purchase-order-details-result', handleSearchResult);
         };
     }, []);
+
+    const [outputFormat, setOutputFormat] = useState('csv');
+    const [remarks, setRemarks] = useState('');
+    const [settingId, setSettingId] = useState(1)
+
+
+    useEffect(() => {
+        if (settingId) {
+            ipcRenderer.send('get-statement-setting-detail', settingId);
+            ipcRenderer.once('statement-setting-detail-data', (event, data) => {
+                if (data) {
+                    setOutputFormat(data.output_format || 'csv');
+                    setRemarks(data.remarks || '');
+                }
+            });
+        }
+    }, [settingId]);
+
+    const handleSave = () => {
+        const settingData = {
+            id: settingId,
+            output_format: outputFormat,
+            remarks: remarks,
+        };
+
+        ipcRenderer.send('save-statement-setting', settingData);
+        ipcRenderer.once('load-statement-settings', (event, data) => {
+            handleConfirmDelete(data); // 更新されたデータを返す
+        });
+    };
+
 
     const toggleDropdown = (id) => {
         console.log(id)
@@ -318,11 +348,78 @@ function Index() {
                     </table>
                 </div>
             </div>
-            <SettingsDialog
-                isOpen={isDialogOpen}
-                onConfirm={handleConfirmDelete}
-                onCancel={handleCancelDelete}
-            />
+            {
+                isDialogOpen &&
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+                    <div className="container mx-auto sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl bg-white rounded-2xl shadow-md">
+                        <p className='text-2xl font-bold px-6 py-4'>明細表設定</p>
+                        <hr />
+                        <div className='flex-col px-6 pt-4'>
+                            <div className=''>出力形式選択</div>
+                            <div className='mt-2.5 flex'>
+                                <label className='text-base'>
+                                    <input
+                                        type="radio"
+                                        name="outputFormat"
+                                        value="csv"
+                                        checked={outputFormat === 'csv'}
+                                        onChange={() => setOutputFormat('csv')}
+                                        className='mr-2'
+                                    />csv
+                                </label>
+                                <label className='text-base ml-10'>
+                                    <input
+                                        type="radio"
+                                        name="outputFormat"
+                                        value="Excel"
+                                        checked={outputFormat === 'Excel'}
+                                        onChange={() => setOutputFormat('Excel')}
+                                        className='mr-2'
+                                    />Excel
+                                </label>
+                                <label className='text-base ml-10'>
+                                    <input
+                                        type="radio"
+                                        name="outputFormat"
+                                        value="PDF"
+                                        checked={outputFormat === 'PDF'}
+                                        onChange={() => setOutputFormat('PDF')}
+                                        className='mr-2'
+                                    />PDF
+                                </label>
+                                <label className='text-base ml-10'>
+                                    <input
+                                        type="radio"
+                                        name="outputFormat"
+                                        value="print"
+                                        checked={outputFormat === 'print'}
+                                        onChange={() => setOutputFormat('print')}
+                                        className='mr-2'
+                                    />印刷
+                                </label>
+                            </div>
+                        </div>
+                        <div className='px-6 pb-4'>
+                            <div className='py-2.5 text-xl'>備考</div>
+                            <div className='pb-2'>
+                                <textarea
+                                    className='border rounded px-4 py-2.5 bg-white w-full resize-none'
+                                    placeholder=''
+                                    rows={5}
+                                    name="remarks"
+                                    value={remarks}
+                                    onChange={(e) => setRemarks(e.target.value)}
+                                ></textarea>
+                            </div>
+                        </div>
+                        <hr />
+                        <div className="flex justify-end py-4 px-6">
+                            <button onClick={handleCancelDelete} className="px-5 py-3 font-semibold text-base mr-6 bg-white border border-gray-300 rounded-xl">キャンセル</button>
+                            <button onClick={handleSave} className="px-11 py-3 font-semibold text-base bg-blue-600 text-white border-0 rounded-xl">保存</button>
+                        </div>
+                    </div>
+                </div>
+            }
         </div>
     )
 }
