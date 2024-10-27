beba47c28bbf77da80c2dca5691dc4aeb2759d04
esakiryota <esaki1217@gmail.com>
2024-10-27 11:40:49 +0900

    fix

--- public/index.js
+++ public/index.js
@@ -2,9 +2,9 @@ const { app, BrowserWindow, ipcMain } = require('electron');
 const path = require('path');
 const sqlite3 = require('sqlite3').verbose();
 
-const {updateElectronApp} = require('update-electron-app');
+// const {updateElectronApp} = require('update-electron-app');
 
-updateElectronApp()
+// updateElectronApp()
 
 //マスタ管理
 require('./ipc/masters/customers'); 
