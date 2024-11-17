68f6a866ccf89e24ed26a8e22594aaac35c86747
os-ryota-a-esaki <os-ryota.a.esaki@rakuten.com>
2024-11-18 06:12:02 +0900

    initialized

--- src/Contact.js
+++ src/Contact.js
@@ -96,10 +96,10 @@ function Contact() {
 
   return (
     <div className="px-8">
-      <div className="relative w-full mt-4 mb-6">
+      <div className="relative w-full mt-4 mb-6 z-10">
         <input
           type="text"
-          className=" pl-10 pr-4 py-2.5 w-full focus:outline-none focus:ring-2 focus:ring-blue-500"
+          className=" pl-10 pr-4 py-2.5 w-full focus:outline-none focus:ring-2 focus:ring-blue-500 z-10"
           placeholder="画面を検索する"
           value={search}
           onChange={handleSearch}
